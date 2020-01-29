b7e169d5033886f36438d6f1a546aecc8cb0efd8
thisistheway369 <thisistheway369@icloud.com>
2020-01-29 05:32:44 -0800

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,48 @@
-def using_include(array, element)
-
-end
-
-def using_sort(array)
-
-end
-
-def using_reverse(array)
-
-end
-
-def using_first(array)
-
-end
-
-def using_last(array)
-
-end
-
-def using_size(array)
-
-end
+def 
+    create_an_empty_array
+    [ ]
+  end
+  
+  def create_an_array
+  animals =  ["Dog","Cat","Rabbit","Horse"]
+  end
+  
+  def add_element_to_end_of_array(array, element)
+    animals =  ["Dog","Cat","Rabbit","Horse"]
+    array << element
+   
+    
+  end
+  
+  def add_element_to_start_of_array(array, element)
+    animals =  ["Dog","Cat","Rabbit","Horse"]
+    array.unshift(element) 
+    
+  end
+  
+  def remove_element_from_end_of_array(array)
+    animals =  ["Dog","Cat","Rabbit","Horse"]
+     array.pop
+    
+  end
+  
+  def remove_element_from_start_of_array(array)
+    animals =  ["Dog","Cat","Rabbit","Horse"]
+    array.shift
+   
+  end
+  
+  def retrieve_element_from_index(array, index_number)
+    
+      array[index_number]
+  end
+  def retrieve_first_element_from_array(array)
+   
+   animals =  ["Dog","Cat","Rabbit","Horse"]
+   array[0]
+  end
+  
+  def retrieve_last_element_from_array(array)
+    array[-1]
+  end
+  
